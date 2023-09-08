071806efcb8914cfeb2e5969a6d1e33e26c516d2
LaylaR <layla.richard.2111@gmail.com>
2023-09-08 11:41:11 +0200

    exercice 2.1

---
+++ 2.1.js
@@ -0,0 +1,14 @@
+﻿const prompt = require('prompt-sync')();
+
+let age= Number(prompt("What's your age?"));
+checkAge(age);
+
+function checkAge(age){
+    if(age >= 18){
+        console.log("You're an adult");
+    }
+    else{
+        console.log("You're not yet an adult");
+    }
+}
+
