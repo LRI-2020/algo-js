23f9b661e4c86a50b67100674ea076b60e8be947
LaylaR <layla.richard.2111@gmail.com>
2023-09-08 14:49:10 +0200

    Exercice 2.7

--- 2.7.js
+++ 2.7.js
@@ -1,2 +1,51 @@
 ﻿const prompt = require('prompt-sync')();
 
+const min = 1;
+const max = 10;
+let count;
+
+do{
+    count = prompt("How much numbers do you want to enter (max :" + max +" )? ");
+
+}while(!isValidInput(count,min,max));
+
+let result = computeSum(Number(count));
+
+console.log("Total sum is " + result);
+
+function computeSum(count)
+{
+    let sum=0;
+
+    for(let i=0; i<count;i++){
+
+        let input;
+
+        do {
+            input = prompt("Enter a number : ");
+        } while (!isNumericString(input));
+
+        sum+= Number(input);
+    }
+    
+    return sum;
+}
+
+
+function isValidInput(input,min, max){
+
+    if(!isNumericString(input)){
+        return false;
+    }
+    return isBetweenLimit(Number(input),min,max);
+}
+function isNumericString(input) {
+    return typeof input === 'string' && !isNaN(input) && !isNullOrEmpty(input);
+}
+function isBetweenLimit(input, min, max){
+
+    return (input >= min && input<=max);
+}
+function isNullOrEmpty(str){
+    return str === '' || str === null || str === undefined || str.trim().length === 0;
+}
