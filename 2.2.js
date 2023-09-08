e822049facbf0c4926834cd96aebf3122a9463ec
LaylaR <layla.richard.2111@gmail.com>
2023-09-08 12:28:50 +0200

    add check min max;

--- 2.2.js
+++ 2.2.js
@@ -1,13 +1,36 @@
 ﻿const prompt = require('prompt-sync')();
 
-let min = askNumberAsInput("Please, enter a number as minimum : ");
+let min, max, current;
+let isMinMaxOk;
 
-let max = askNumberAsInput("Please, enter a number as maximum : ");
+min = askNumberAsInput("Please, enter a number as minimum : ");
 
-let current = askNumberAsInput("Please, enter a number to compare with min and max: ");
+do {
+    max = askNumberAsInput("Please, enter a number as maximum : ");
+    
+    isMinMaxOk = checkMinMax(min,max);
+}
+while(!isMinMaxOk);
+
+
+current = askNumberAsInput("Please, enter a number to compare with min and max: ");
 
 checkCurrent(min,max,current);
 
+// functions
+function checkMinMax(min,max){
+    if(isMinInfToMax(min,max)){
+        return true;
+    }
+    
+    else{
+        console.log("Min should be < Max");
+        return false;
+    }
+}
+function isMinInfToMax(min, max){
+    return min < max;
+}
 function askNumberAsInput(message){
    let input, isANumber;
    
@@ -23,11 +46,11 @@ function askNumberAsInput(message){
 function checkCurrent(min, max, current){
 
     if(current > min && current<max){
-        console.log(current + "is between " + min + " and " + max + ".");
+        console.log(current + " is between " + min + " and " + max + ".");
     }
 
     else{
-        console.log(current + "is not between " + min + " and " + max + ".");
+        console.log(current + " is not between " + min + " and " + max + ".");
     }
 }
 function isNumericString(input) {
