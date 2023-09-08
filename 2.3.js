c5d2e4262cf618eee9820fe1bfc1963b5197bdb3
LaylaR <layla.richard.2111@gmail.com>
2023-09-08 13:04:42 +0200

    Exercice 2.3 bonus

--- 2.3.js
+++ 2.3.js
@@ -1,17 +1,28 @@
 ﻿const prompt = require('prompt-sync')();
 
-// for(let i=1;i<=100;i++){
-//     if (i%2===0){
+// for version - 50 executions
+
+let count = 0;
+
+for(let i=2;i<=100;i++){
+    if (i%2===0){
+        console.log(i);
+        i++
+    }    
+    count++;
+    if(i>=100){
+        console.log("This loop has been executed " + count + " times.");
+    }
+}
+
+// While version - 100 executions
+
+// let i = 1;
+//
+// while(i<=100){
+//     if(i%2===0){
 //         console.log(i);
 //     }
+//
+//     i++;
 // }
-
-let i = 1;
-
-while(i<=100){
-    if(i%2===0){
-        console.log(i);
-    }
-    
-    i++;
-}
