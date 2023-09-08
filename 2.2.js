7a1f06385a883bded6bb68368549ad1e31138fce
LaylaR <layla.richard.2111@gmail.com>
2023-09-08 12:17:08 +0200

    Exercice 2.2

---
+++ 2.2.js
@@ -0,0 +1,40 @@
+﻿const prompt = require('prompt-sync')();
+
+let min = askNumberAsInput("Please, enter a number as minimum : ");
+
+let max = askNumberAsInput("Please, enter a number as maximum : ");
+
+let current = askNumberAsInput("Please, enter a number to compare with min and max: ");
+
+checkCurrent(min,max,current);
+
+function askNumberAsInput(message){
+   let input, isANumber;
+   
+   do {
+        input = prompt(message);
+        isANumber = isNumericString(input);
+    }
+    while(!isANumber);
+   
+   return Number(input);
+}
+
+function checkCurrent(min, max, current){
+
+    if(current > min && current<max){
+        console.log(current + "is between " + min + " and " + max + ".");
+    }
+
+    else{
+        console.log(current + "is not between " + min + " and " + max + ".");
+    }
+}
+function isNumericString(input) {
+    return typeof input === 'string' && !isNaN(input) && !isNullOrEmpty(input);
+}
+
+function isNullOrEmpty(str){
+    
+    return str === '' || str === null || str === undefined || str.trim().length === 0;
+}
