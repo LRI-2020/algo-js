67e8a950c9af46279f0891e738d8bf09bdfbf250
LaylaR <layla.richard.2111@gmail.com>
2023-09-09 12:43:06 +0200

    correct main path remove public directory correct server express path for test

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 ﻿const express = require("express");
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('./'));
 
 app.get("/",function (req, res){
     res.sendFile(__dirname+"/index.html");
