401794bed07477653d1bed2d224536108b5ee97f
LaylaR <layla.richard.2111@gmail.com>
2023-09-11 15:27:49 +0200

    Exercice 4.6

--- assets/js/4.6.js
+++ assets/js/4.6.js
@@ -1,6 +1,29 @@
-﻿
+﻿import {askForNumericString, isPositiveIntegerString} from "./Helpers.js";
+
 function runExercice4_6() {
-       
+    
+    let input;
+    
+    do{
+       input= prompt("Enter the number of which you want the factorial") 
+    }while (!isPositiveIntegerString(input));
+    
+    alert("The factorial of " + input + " is " + factorial(Number(input)));    
+}
+
+function factorial(n){
+    
+    if(n === 0){
+        return 1;
+    }
+    let result = 1;
+    
+    for(let i = n; i>0;i--){
+        
+        result*=i;
+    }
+    
+    return result;
 }
 
 export {runExercice4_6};
--- assets/js/Helpers.js
+++ assets/js/Helpers.js
@@ -1,12 +1,10 @@
-﻿
-
-function askForNumericString(message){
+﻿function askForNumericString(message) {
 
     let input;
-    do{
+    do {
         input = prompt(message);
 
-    }while(!isNumericString(input));
+    } while (!isNumericString(input));
 
     return input;
 }
@@ -21,30 +19,43 @@ function rand10() {
 function randMinMax(min, max) {
 
     min = Math.ceil(min);
-    max = Math.floor(max+1);
+    max = Math.floor(max + 1);
     return Math.floor(Math.random() * (max - min) + min);
 }
 
 
-function isValidNumericInput(input, min, max){
+function isValidNumericInput(input, min, max) {
 
-    if(!isNumericString(input)){
+    if (!isNumericString(input)) {
         return false;
     }
-    return isBetweenLimit(Number(input),min,max);
+    return isBetweenLimit(Number(input), min, max);
 }
 
-function isBetweenLimit(input, min, max){
+function isBetweenLimit(input, min, max) {
 
-    return (input >= min && input<=max);
+    return (input >= min && input <= max);
 }
 
 function isNumericString(input) {
     return typeof input === 'string' && !isNaN(input) && !isNullOrEmpty(input);
 }
 
+function isPositiveIntegerString(input) {
+    if (isNumericString(input)) {
 
-function isNullOrEmpty(str){
+        if (Number.isInteger(Number(input)) && Number(input) >= 0) {
+            return true;
+        }
+        alert("Number should be > 0 and an integer")
+    }
+
+    return false;
+}
+
+
+function isNullOrEmpty(str) {
     return str === '' || str === null || str === undefined || str.trim().length === 0;
 }
-export{ isNullOrEmpty, isNumericString, isValidNumericInput, isBetweenLimit, askForNumericString, rand10, randMinMax};
+
+export {isPositiveIntegerString, isNullOrEmpty, isNumericString, isValidNumericInput, isBetweenLimit, askForNumericString, rand10, randMinMax};
