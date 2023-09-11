8b784c1451b10d9a8a3e2e6507aa8b70a507bcaf
LaylaR <layla.richard.2111@gmail.com>
2023-09-11 12:08:43 +0200

    Exercice 4.1

--- assets/js/4.1.js
+++ assets/js/4.1.js
@@ -1,6 +1,15 @@
-﻿
+﻿import {askForNumericString} from "./Helpers.js";
 function runExercice4_1() {
-       
+
+    let width = askForNumericString("What is the width of your rectangle ?");
+    let length = askForNumericString("What is the lenght of your rectangle ?");
+    
+    alert("The surface of your rectangle is : " + calcSurface(width, length));  
+}
+
+function calcSurface(width,length){
+    
+    return width*length;
 }
 
 export {runExercice4_1};
--- assets/js/Helpers.js
+++ assets/js/Helpers.js
@@ -1,4 +1,16 @@
 ﻿
+
+function askForNumericString(message){
+
+    let input;
+    do{
+        input = prompt(message);
+
+    }while(!isNumericString(input));
+
+    return input;
+}
+
 function isValidNumericInput(input, min, max){
 
     if(!isNumericString(input)){
@@ -20,4 +32,4 @@ function isNumericString(input) {
 function isNullOrEmpty(str){
     return str === '' || str === null || str === undefined || str.trim().length === 0;
 }
-export{ isNullOrEmpty, isNumericString, isValidNumericInput, isBetweenLimit};
+export{ isNullOrEmpty, isNumericString, isValidNumericInput, isBetweenLimit, askForNumericString};
