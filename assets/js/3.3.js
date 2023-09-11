d7079f99e934ac101ba2d158e2388ec38f358337
LaylaR <layla.richard.2111@gmail.com>
2023-09-11 10:40:40 +0200

    Exercice 3.3

--- assets/js/3.3.js
+++ assets/js/3.3.js
@@ -1,5 +1,44 @@
 ﻿
 
-function runExercice3_3(){}
+function runExercice3_3(){
+    
+    let originalArray = ["apple","peach","strawberry"];
+    
+    let copyUsingPush = copyWithPush(originalArray);
+    
+    let copyUsingSlice = copyWithSlice(originalArray);
+
+    console.log("The orginal array is : ");
+    readArray(originalArray);
+    
+    console.log("The copy with push method is : ");
+    readArray(copyUsingPush);
+
+    console.log("The copy with slice method is : ");
+    readArray(copyUsingSlice);
+    
+}
+
+function copyWithPush(array){
+    let newArray = [];
+    
+    for(let item of array){
+        
+        newArray.push(item);
+    }
+    
+    return newArray;
+}
+
+function copyWithSlice(array){
+    return array.slice();
+}
+
+function readArray(array){
+
+    for(let item of array){
+        console.log(item);
+    }
+}
 
 export {runExercice3_3};
