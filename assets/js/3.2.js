954b3eaaf85d1a9a3b241364ea1fcde6b1df9706
LaylaR <layla.richard.2111@gmail.com>
2023-09-11 09:51:52 +0200

    Exercice 3.2

--- assets/js/3.2.js
+++ assets/js/3.2.js
@@ -1,5 +1,26 @@
 ﻿
 
-function runExercice3_2(){}
+function runExercice3_2(){    
+    
+    let arr1 = [1,2,3,4,5];
+    let arr2 = [100,101,102];
+
+    alert(`The avg of first array = ${runAvg(arr1)}
+The avg of second array = ${runAvg(arr2)}`);
+}
+
+function runAvg(arr1) {
+
+    let sum=0;
+    let itemsCount= arr1.length;
+    
+    for (let item of arr1){
+
+        sum+=item;
+    }
+
+    return sum/itemsCount;
+}
+
 
 export {runExercice3_2};
