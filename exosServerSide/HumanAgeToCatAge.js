12495fb992f6a1ccb886d31e8dc123d630c33052
LaylaR <layla.richard.2111@gmail.com>
2023-09-13 10:38:51 +0200

    HumanToCat

---
+++ exosServerSide/HumanAgeToCatAge.js
@@ -0,0 +1,30 @@
+﻿const helpers = require("./Helpers.js");
+const prompt = require('prompt-sync')();
+
+let input
+
+do {
+    input = prompt("Enter your age (min 0 and max 125) : ");
+}while(!helpers.isValidNumericInput(input,0,125));
+
+console.log("You are " + calcAgeToCatYears(Number(input)) + " years old in cat age");
+
+
+
+function calcAgeToCatYears(input) {
+
+    let earlyYears ;
+    let otherYears = 0;
+
+    if(input <=2){
+
+        earlyYears=input*25;
+    }
+    
+    else{
+        earlyYears=2*25;
+        otherYears=(input-2)*4;
+    }
+
+    return earlyYears+otherYears;
+}
